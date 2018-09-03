2cc35843836408aee422ac8f86bddfb367ccebf3
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-09-03 10:32:45 -0400

    Make Travis auto deploy
    The test showed that netlify was still building the app but not
    deploying it automatically. This will be done using the new webhooks. It
    will be tested with this push.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-
-# die on error
-set -e
-
-# https://gist.github.com/cjus/1047794
-echo 'Retrieving latest deploy...'
-url=`curl -H "Authorization: Bearer $netlify_token" https://api.netlify.com/api/v1/sites/blissful-noether-dca508/deploys`
-temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
-
-# https://www.netlify.com/docs/api/#deploys
-echo "Publishing build ${temp##*|}..."
-curl -X POST -H "Authorization: Bearer $netlify_token" -d "{}" "https://api.netlify.com/api/v1/sites/blissful-noether-dca508/deploys/${temp##*|}/restore"
+set -e # halt script on error 
+ls
+zip -r website.zip build 
+curl -H "Content-Type: application/zip" \ 
+    -H "Authorization: Bearer $netlify_token" \ 
+    --data-binary "@website.zip" \ 
+    https://api.netlify.com/api/v1/sites/blissful-noether-dca508.netlify.com/deploys
