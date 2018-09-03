8a03f3b80ccc3a332e7ff70c30fada7bad2dcd1e
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-09-03 11:13:43 -0400

    Deployement via Netlify automation
    Netlify can automatically deploy when a push comes to master. Locking
    the master branch from pushes and using PRs then Travis and other CI
    tools to verify them. Maybe locking PRs from being resolved until all
    checks pass.
    
    Travis bundling and sending files to Netlify is not functioninal with
    curl. Instead of time debugging using the basic features of Netlify will
    be preferable.

--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error 
-ls
-zip -r website.zip build 
-curl -H "Content-Type: application/zip" \ 
-    -H "Authorization: Bearer $netlify_token" \ 
-    --data-binary "@website.zip" \ 
-    https://api.netlify.com/api/v1/sites/blissful-noether-dca508.netlify.com/deploys
