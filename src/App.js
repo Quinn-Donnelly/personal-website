fe48eecda46796561ca0f867db8fb097ad9e1794
Quintin Donnelly <quinndonnelly22@gmail.com>
2018-09-02 23:11:55 -0400

    Testing Full Pipeline
    Need to test push the pipeline to ensure all parts are working.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Let's see this workflow in action not bad
+          Vivi this is why code is cool!
         </p>
       </div>
     );
