1fb6e4fb1ca7b607d37b481f18c17aa4233e17c8
Quintin Donnelly <quinndonnelly22@gmail.com>
2020-03-14 20:14:46 -0400

    Fix the css width overflow
    The page had 10px off screen causing janking nav for horzontail
    scrolling

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,29 +2,33 @@ import React, { Component } from 'react'
 import './HomePage.css';
 import { Link } from 'react-router-dom';
 import SocialButton from '../../components/SocialButton/SocialButton';
+import ProjectPage from "../ProjectPage/ProjectPage";
 
 
 export default class Home extends Component {
   render() {
     return (
-      <div id="HomePage" className="HomePage">
-        <header className="center">
-          <h1>Quintin Donnelly</h1>
-          <h4>Continously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
-          <Link to="/quintin-donnelly-resume.pdf" target="_blank">Open for Opportunities</Link>
-        </header>
+        <>
+          <div id="HomePage" className="HomePage">
+            <header className="center">
+              <h1>Quintin Donnelly</h1>
+              <h4>Continously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
+              <Link to="/quintin-donnelly-resume.pdf" target="_blank">Open for Opportunities</Link>
+            </header>
 
-        <section className="bottom-message">
-          <h4 className="">This site is currently under construction...</h4>
-        </section>
+            <section className="bottom-message">
+              <h4 className="">This site is currently under construction...</h4>
+            </section>
 
-        <div className="floating-toolbar">
-          <SocialButton type="github" />
-          <SocialButton type="linkedin" />
-          <SocialButton type="twitter" />
-          <SocialButton type="email" />
-        </div>
-      </div>
+            <div className="floating-toolbar">
+              <SocialButton type="github" />
+              <SocialButton type="linkedin" />
+              <SocialButton type="twitter" />
+              <SocialButton type="email" />
+            </div>
+          </div>
+            <ProjectPage />
+        </>
     )
   }
 }
---
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProjectPage.css';
+
+const ProjectPage: React.FunctionComponent = () => {
+    return (
+        <div id="project-page">This is the project page tests</div>
+    );
+};
+
+export default ProjectPage;
