f148e218dd1be25e337c992da290036018fef0b1
Quinn Donnelly <quinndonnelly22@gmail.com>
2021-02-13 11:57:09 -0500

    Fix type add typescript types

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 import SocialButton from '../../components/SocialButton/SocialButton';
 
 
-export default class Home extends Component {
+export default class Home extends Component<any, any> {
   render() {
     return (
       <div id="HomePage" className="HomePage">
         <header className="center">
           <h1>Quintin Donnelly</h1>
-          <h4>Continously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
+          <h4>Continuously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
           <Link to="/quintin-donnelly-resume.pdf" target="_blank">Open for Opportunities</Link>
         </header>
 
