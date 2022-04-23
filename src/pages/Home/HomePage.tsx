ae8dccb63e3ea721276b55a4dee07581bde8a2d0
Quinn Donnelly <quinndonnelly22@gmail.com>
2022-04-23 10:48:15 -0400

    Fix typo and remove open for oppurtunity

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -10,8 +10,7 @@ export default class Home extends Component {
       <div id="HomePage" className="HomePage">
         <header className="center">
           <h1>Quintin Donnelly</h1>
-          <h4>Continously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
-          <Link to="/quintin-donnelly-resume.pdf" target="_blank">Open for Opportunities</Link>
+          <h4>Continuously <strong className="secondary">Learning</strong> and Constantly <strong className="primary">Hacking</strong>.</h4>
         </header>
 
         <section className="bottom-message">
