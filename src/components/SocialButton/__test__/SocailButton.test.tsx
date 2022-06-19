dcd46dc4fac1589544ade20e75a8de0cc5763364
Quinn Donnelly <quinndonnelly22@gmail.com>
2022-06-19 12:40:00 -0400

    Updated outdated deps and moved back to create-react-app template

--- src/components/SocialButton/__test__/SocailButton.test.tsx
+++ src/components/SocialButton/__test__/SocailButton.test.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import { shallow, mount, ShallowWrapper } from 'enzyme';
-import { Link } from 'react-router-dom';
 import SocialButton from '../SocialButton';
-import SocialInformation from '../SocialInformation';
+import {render, screen} from "@testing-library/react";
 
 describe('<SocailButton />', () => {
-  it('should link to the appropriate url', () => {
-    const button = shallow(<SocialButton type="email"/>);
-  
-    const correctInfo = SocialInformation.get('email');
-    if (!correctInfo) {
-      fail();
-      return;
-    }
+  it('Should render an icon', () => {
+    render(<SocialButton type="github" />);
+    const aTag = screen.getByRole("link");
 
-    expect(button.find('a').prop('href')).toEqual(correctInfo.url);
-  });
-
-  it('should render nothing if no scail link found', () => {
-
-    const Test = () => <></>
-
-    const button = shallow(<SocialButton type="asdf"/>);
-    const empty = shallow(<Test />);
-
-    expect(button).toEqual(empty);    
-  });
+    expect(aTag).toBeInTheDocument();
+    expect(aTag).toHaveAttribute("href", "https://github.com/Quinn-Donnelly");
+  })
 })
